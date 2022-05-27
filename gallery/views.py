ab6c369c3f3c8aa97e25acd5bfb0c3c425e24f1b
Shalyne-Waweru <shalynewaweru@gmail.com>
2022-05-27 19:22:20 +0300

    Add and implement the templates, static files and bootstrap

--- gallery/views.py
+++ gallery/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http  import HttpResponse
 
-# Create your views here.
+def home(request):
+  '''
+  View function that returns the home page and its data
+  '''
+  return render(request, 'index.html', {})
