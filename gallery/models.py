5e555667a04ae4fd727ef1ddef863442f87c9552
Shalyne-Waweru <shalynewaweru@gmail.com>
2022-05-28 16:31:04 +0300

    Write and run tests for the Location model

--- gallery/models.py
+++ gallery/models.py
@@ -18,6 +18,12 @@ class Location(models.Model):
     def __str__(self):
         return self.locationName
 
+    def save_location(self):
+        self.save()
+
+    def delete_location(self):
+        self.delete()
+
 class Image(models.Model):
   image = models.ImageField(upload_to = 'gallery-images/',default='DEFAULT VALUE')
   title = models.CharField(max_length =30)
--- gallery/tests.py
+++ gallery/tests.py
@@ -49,3 +49,51 @@ class CategoryTestClass(TestCase):
         self.new_category.save_category()
         self.new_category.delete_category()
         self.assertTrue(len(Category.objects.all()) == 0)
+
+class LocationTestClass(TestCase):
+    '''
+    Test class that defines test cases for the model behaviours.
+
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    '''
+    def setUp(self):
+        '''
+        Set up method to run before each test cases.
+        It defines instructions that will be executed before each test method.
+        '''
+        self.new_location = Location(locationName = 'Nairobi')
+
+    # FIRST TEST
+    def test_instance(self):
+        '''
+        test_instance test case to test if the object is initialized properly
+        '''
+        self.assertTrue(isinstance(self.new_location,Location))
+
+    # SECOND TEST
+    def test_save_location(self):
+        '''
+        test_save_location test case to test if the object is being saved correctly
+        '''
+        self.new_location.save_location()
+        locations = Location.objects.all()
+        self.assertTrue(len(locations) > 0)
+
+    # THIRD TEST
+    def test_update_location(self):
+        '''
+        test_update_location test case to test if the object is being updated correctly
+        '''
+        self.new_location.save_location()
+        self.new_location.locationName = 'Nrb'
+        self.assertTrue(self.new_location.locationName == 'Nrb')
+
+    # FOURTH TEST
+    def test_delete_loaction(self):
+        '''
+        test_delete_loaction test case to test if the object is being deleted correctly
+        '''
+        self.new_location.save_location()
+        self.new_location.delete_location()
+        self.assertTrue(len(Location.objects.all()) == 0)
