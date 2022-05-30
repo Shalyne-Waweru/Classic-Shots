ae820f6f89f4f8380ed64dd52f23b0adf7621d52
Shalyne-Waweru <shalynewaweru@gmail.com>
2022-05-30 12:36:48 +0300

    Modify the modal appearance and implement the close button functionality

--- static/js/script.js
+++ static/js/script.js
@@ -15,33 +15,3 @@ $(document).ready(function(){
 
 });
 
-// Get the modal
-var modal = document.getElementById('my-Modal');
-
-// Get the image and insert it inside the modal
-var img = document.getElementsByClassName('gallery-img');
-// var captionText = document.getElementById("caption").textContent;
-// alert(captionText);
-
-for(var i=0; i<img.length; i++){
-
-  var modalImg = document.getElementById("img01");
-  var modalCaption = document.getElementById("modal-caption");
-  
-  img[i].addEventListener('click',function(){
-      modal.style.display = "block";
-      modalImg.src = this.src;
-      // modalCaption.innerHTML = captionText;
-      
-      // alert(captionText.textContent);
-  })
-}
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
