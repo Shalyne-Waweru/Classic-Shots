e46f1d4dde304145f90c17c0e6c08b8278830342
Shalyne-Waweru <shalynewaweru@gmail.com>
2022-05-30 21:21:03 +0300

    Remove the STATICFILES_STORAGE property

---
+++ staticfiles/js/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+  $(".tooltiptext").hide();
+
+  // Share Button Hover Effect
+  $(".shareBtn").hover(function(){
+    $(".tooltiptext").toggle();
+  });
+
+  $(".shareBtn").click(function(){
+    $(".tooltiptext").html("Copied to Clipboard!");
+    $(".tooltiptext").css("background-color","#0979A5");
+
+  });
+
+  $(".gallery-img").click(function(){
+    $(".fetch-response").hide();
+  })
+
+});
+
